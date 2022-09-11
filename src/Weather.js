dba4e6f0aa66a5cf4dc7616e34cebca921bd4a47
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2022-09-11 15:42:19 -0500

    Completed JSX and CSS
    Finished JSX and CSS. On to React JS

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,8 @@ const Weather = () => {
   return (
     <div className="container">
         <img />
+        <div className="time">1:00pm</div>
+        <div className="condition">Sunny</div>
         <div className="temp">60</div>
     </div>
   )
