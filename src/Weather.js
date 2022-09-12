a093f0d15ec9bb09eb00cc34abff994f2ffb8797
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2022-09-12 17:35:08 -0500

    Updated with JSX and CSS
    Added some bottom data and time, date , and location

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,46 @@
+import { useState, useEffect } from "react"
+
 const Weather = () => {
+   const [data, setData] = useState({});
+   const [location, setLocation] = useState('');
+
+   // Getting time
+   const currDate = new Date().toLocaleDateString();
+   const currTime = new Date().toLocaleTimeString(
+    'en-US', {hour: '2-digit', minute: '2-digit'});
+
+
+ 
+
+
   return (
     <div className="container">
-        <img />
-        <div className="time">1:00pm</div>
+        <div className="data">
+        <input
+        placeholder="Enter Location"
+        className="InputData"
+        type="search"
+        />
+        <div className="date">{currDate}</div>
+        <div className="time">{currTime}</div>
+        <div className="location">Lafayette</div>
         <div className="condition">Sunny</div>
         <div className="temp">60</div>
+        </div>
+        <div className="bottom-data">
+          <div className="feels">
+            <p className="bold">65F</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
     </div>
   )
 }
